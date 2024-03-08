8dfdb4cd795a4393207a2f2d304e321a3b5ab892
Alexis25yo <alexis25yo@gmail.com>
2024-03-08 17:59:17 -0500

    added 04-homehork

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import "./style.css";
 import "./topics/01-basic-types"
 import "./topics/02-object-interface"
-import "./topics/0.3-functions"
+import "./topics/03-functions"
+import "./topics/04-homework"
 
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
---
+++ src/topics/03-functions.ts
@@ -0,0 +1,44 @@
+
+
+
+function addNumbers( a: number, b: number ){
+    return a + b;
+}
+
+const addNumbersArrow = ( a: number, b: number ):string => {
+    return `${a + b}`;
+}
+
+function Multiply( firstNumber: number, secondNumber?: number, base: number = 2 ){
+    return firstNumber * base;
+}
+
+// const result: number = addNumbers(1, 2);
+// const result2: string = addNumbersArrow(1, 2);
+// const MultiplyResult: number = Multiply(5);
+// console.log({ result, result2, MultiplyResult });
+
+interface Character{
+    name: string;
+    hp: number;
+    showHP: () => void;
+}
+
+const healCharacter = ( character: Character, amount: number ) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHP() {
+        console.log(`Puntos de vida ${this.hp}`);
+        
+    },
+}
+
+healCharacter(strider, 10); 
+healCharacter(strider, 30); 
+strider.showHP();
+
+export {};
